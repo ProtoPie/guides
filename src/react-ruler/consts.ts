712236808000b94afb39dd9971935c29cce6be3e
Dmytro Koval <d.koval@protopie.io>
2022-12-07 18:48:48 +0200

    feat: KS-161 ruler library added

---
+++ src/react-ruler/consts.ts
@@ -0,0 +1,32 @@
+import { RulerInterface, RulerProps } from './types';
+
+export const PROPERTIES: Array<keyof RulerProps> = [
+    'type',
+    'width',
+    'height',
+    'unit',
+    'zoom',
+    'direction',
+    'textAlign',
+    'font',
+    'segment',
+    'mainLineSize',
+    'longLineSize',
+    'shortLineSize',
+    'lineOffset',
+    'textOffset',
+    'negativeRuler',
+    'range',
+    'scrollPos',
+    'style',
+    'backgroundColor',
+    'rangeBackgroundColor',
+    'lineColor',
+    'textColor',
+    'textBackgroundColor',
+    'textFormat',
+    'portalContainer',
+];
+export const METHODS: Array<keyof RulerInterface> = [
+    'scroll', 'resize',
+];
